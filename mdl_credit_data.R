5ff9ae11276ade28a55e915afabf8122cff74bcf
factorialmap <dbabr@hotmail.com>
2021-08-14 09:49:34 -0300

    teste

---
+++ mdl_credit_data.R
@@ -0,0 +1,19 @@
+
+# goals -------------------------------------------------------------------------------------------------
+"criar modelo para classificar corretamente emprestimos bons e ruins"
+
+
+# packages ----------------------------------------------------------------------------------------------
+library(modeldata)
+library(tidymodels)
+library(tidyverse)
+library(funModeling)
+library(vip)
+library(janitor)
+
+
+# data --------------------------------------------------------------------------------------------------
+data("credit_data")
+
+
+
